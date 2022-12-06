f50e2fb99a8f5a97af6c7ce2aa44cb40bd9fe9c1
Alinevictoriasouza <alynevictoriasouza47@gmail.com>
2022-12-06 16:49:48 -0400

    modificado alguns nomes

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
-const CACHE_NAME = "APP-RGSHOP";
+const CACHE_NAME = "Livraria-Online";
 const OFFLINE_URL = "/fallback";
-
+// Customize this with a different URL if needed.
 self.addEventListener("install", (event) => {
     event.waitUntil(
         (async () => {
             const cache = await caches.open(CACHE_NAME);
+            // Setting {cache: 'reload'} in the new request will ensure that the response
+            // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
             await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
         })()
     );
